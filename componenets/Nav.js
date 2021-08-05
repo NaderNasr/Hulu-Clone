ac5a956111406ceb72cb1d01ec5db83729a5c940
Nader Nasr <nadernasr7@gmail.com>
2021-08-05 00:44:44 -0400

    genre navigation complete

--- componenets/Nav.js
+++ componenets/Nav.js
@@ -1,9 +1,25 @@
+import { useRouter } from "next/dist/client/router";
+import requests from "../utils/requests";
+
 function Nav() {
-    return (
-        <div>
-            
-        </div>
-    )
+    const router = useRouter()
+  return (
+    <nav className='relative'>
+      <div className="flex px-10 sm:px-20 scrollbar-hide text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-auto">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            onClick={() => router.push(`/?genre=${key}`)}
+            className="last:pr-24 cursor-pointer transition 
+            transform hover:scale-125 hover:text-red-500"
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div className='absolute top-0 right-0 bg-gradient-to-l from-blue-900 h-10 w-1/2'/>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
